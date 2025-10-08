2cc5866c10c5a6eeffdeee6753f91b0d44aa6eb7
Hussein Naeem <husseinnaeemsec@gmail.com>
2025-10-08 15:38:52 +0300

    .

--- src/pages/student/ClassroomPage.tsx
+++ src/pages/student/ClassroomPage.tsx
@@ -1,8 +1,8 @@
 // pages/ClassroomPage.tsx
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import type {EnrollmentLecture } from "../../../types";
-import { useAppDispatch} from "../../store/store";
+import type { EnrollmentLecture } from "../../../types";
+import { useAppDispatch, useAppSelector } from "../../store/store";
 import api from "../../api/client";
 import { endpoints } from "../../api/routes";
 import ResourceLoader from "../../components/resourceLoader";
@@ -11,15 +11,17 @@ import NetworkError from "../../components/errors/NetworkError";
 import { setActiveLecture, setEnrollment } from "../../store/enrollmentSlice";
 import EnrollmentNavigation from "../../components/enrollment/EnrollmentNavigation";
 import CourseContent from "../../components/enrollment/CourseContent";
+import { setUser } from "../../store/authSlice";
 
 export default function ClassroomPage() {
   const { enrollment_id } = useParams<{ enrollment_id: string }>();
+  const { user } = useAppSelector(state => state.auth);
   const enrollmentIdNum = enrollment_id ? Number(enrollment_id) : null;
   const dispatch = useAppDispatch();
-  const [loading,setLoading] = useState(true);
-  const [error,setError] = useState<'network_error'|'not_found'|'rate_limiting'|null>(null)
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<'network_error' | 'not_found' | 'rate_limiting' | null>(null)
 
-  if(!enrollmentIdNum){
+  if (!enrollmentIdNum) {
     return (
       <div className="w-full h-dynamic flex items-center justify-center flex-col gap-2">
         <h1 className="text-3xl text-rose-600 font-semibold"> خطأ </h1>
@@ -30,48 +32,58 @@ export default function ClassroomPage() {
 
 
 
-  useEffect(()=>{
+  useEffect(() => {
     api.get(endpoints.user.enrollments.getEnrollment(enrollmentIdNum))
-    .then((res)=> {
-      dispatch(setEnrollment(res.data))
-      const first_lecture : EnrollmentLecture | null = res.data.course?.sections[0]?.lectures[0] || null
-      const last_watched_lecture : EnrollmentLecture | null = res.data.last_watched_lecture || first_lecture
-      dispatch(setActiveLecture(last_watched_lecture));
+      .then((res) => {
+        dispatch(setEnrollment(res.data));
+        // if (user) {
+        //   dispatch(setUser({
+        //     ...user,
+        //     progress: {
+        //       ...user.progress,
+        //       last_watched_enrollment: res.data,
+        //     },
+        //   }));
+        // }
 
-    })
-    .catch((e)=>{
-      if(e.status === 404){
-        setError('not_found')
-      }else if(e.status === 500){
-        setError('network_error')
-      }
-    })
-    .finally(()=> setLoading(false) )
-  },[])
+        const first_lecture: EnrollmentLecture | null = res.data.course?.sections[0]?.lectures[0] || null
+        const last_watched_lecture: EnrollmentLecture | null = res.data.last_watched_lecture || first_lecture
+        dispatch(setActiveLecture(last_watched_lecture));
 
-  if(loading){
-    return <ResourceLoader className="h-dynamic" title="جاري تحميل الدورة التدريبية"  />
+      })
+      .catch((e) => {
+        if (e.status === 404) {
+          setError('not_found')
+        } else if (e.status === 500) {
+          setError('network_error')
+        }
+      })
+      .finally(() => setLoading(false))
+  }, [])
+
+  if (loading) {
+    return <ResourceLoader className="h-dynamic" title="جاري تحميل الدورة التدريبية" />
   }
 
-  if(error){
-    switch(error){
+  if (error) {
+    switch (error) {
       case 'not_found':
         return <NotFoundError className="h-dynamic" showText />;
-      
+
       case 'network_error':
         return <NetworkError className="h-dynamic" showText />;
-      
+
     }
   }
 
   return (
-      <div className="space-y-8 dark:bg-emerald-950 bg-white">
-        <div className="bg-white dark:bg-transparent rounded-lg space-y-5">
-          <div className="grid border px-8 lg:grid-cols-[1fr_30%]  h-full overflow-y-hidden">
-            <CourseContent />
-            <EnrollmentNavigation />
-          </div>
+    <div className="space-y-8 dark:bg-emerald-950 bg-white">
+      <div className="bg-white dark:bg-transparent rounded-lg space-y-5">
+        <div className="grid border px-8 lg:grid-cols-[1fr_30%]  h-full overflow-y-hidden">
+          <CourseContent />
+          <EnrollmentNavigation />
         </div>
       </div>
+    </div>
   );
 }
