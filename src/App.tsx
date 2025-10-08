1d37af6e58f0db1480e6d1c1f691a12ffb2d7c7d
Hussein Naeem <husseinnaeemsec@gmail.com>
2025-10-08 15:35:12 +0300

    calling useServerWorker hook

--- src/App.tsx
+++ src/App.tsx
@@ -2,27 +2,20 @@ import { Outlet } from 'react-router-dom';
 import { useAppDispatch, useAppSelector } from './store/store';
 import { useEffect } from 'react';
 import { setupUserPrefrences } from './utils/functions';
-import useNotifications from './hooks/use-notifications';
 import api from './api/client';
 import { endpoints } from './api/routes';
 import { setNotifications } from './store/authSlice';
 import NotificationsPopUp from './components/NotificationsPopUp';
+import useServerWorker from './hooks/use-server-worker';
 
 const App = () => {
   const { isAuthenticated, user } = useAppSelector((state) => state.auth);
-  const {ready,sendMessage,loading:ws_loading} = useNotifications();
+  const {} = useServerWorker();
   const dispatch = useAppDispatch()
-
   useEffect(() => {
     if (isAuthenticated && user) {
       api.get(endpoints.notifications.list).then( res => dispatch(setNotifications(res.data.results)))
 
-      if(!ws_loading){
-        console.log("Ready")
-        sendMessage({
-          title:"Test message"
-        })
-      }
       try {
         setupUserPrefrences(user);
       } catch (err) {
