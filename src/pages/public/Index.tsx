5eda7016b221e5b67d01d378d6472196d6cd4a7b
Hussein Naeem <husseinnaeemsec@gmail.com>
2025-10-14 06:32:52 +0300

    .

--- src/pages/public/Index.tsx
+++ src/pages/public/Index.tsx
@@ -2,22 +2,24 @@ import { Outlet } from "react-router-dom";
 import Features from "../../components/landing/Features";
 import HeroSection from "../../components/landing/HeroSection";
 import TopNavigation from "../../components/landing/TopNavigation";
+import Footer from "./Footer";
 
 export default function LandingPage(){
     return (
-        <div>
+        <>
             <TopNavigation />
             <Outlet />
-        </div>
+            <Footer />
+        </>
     )
 }
 
 
 export function Home(){
     return (
-        <>
+        <div className="bg-white">
             <HeroSection />
             <Features />
-        </>
+        </div>
     )
 }
