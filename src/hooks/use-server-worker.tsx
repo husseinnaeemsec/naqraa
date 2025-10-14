3e670de6702ea4adf4e81a39161de3d27f1fca94
Hussein Naeem <husseinnaeemsec@gmail.com>
2025-10-14 06:32:28 +0300

    Cleaning logs after testing the push notifications `note that we need to optamize this for un-authenticated users so we don't overhit the api for subjscriptions , also we need to optamize caching for the subscriptions, this is a test branch so the `PPK` variable is not a security issue

--- src/hooks/use-server-worker.tsx
+++ src/hooks/use-server-worker.tsx
@@ -2,34 +2,28 @@
 import { useEffect } from "react";
 import { urlBase64ToUint8Array } from "../utils/urlBase64ToUint8Array";
 
+const PPK = 'BKBqLHq78vADus1WZXiTDwXz-79jpRzjpINFHxLY4n93sJOLtIS17cv_Ge6xuPfcJTxZBsBscvnzpGzYKkdawbU';
+
 export default function useServerWorker() {
   useEffect(() => {
     async function registerAndSubscribe() {
       if (!("serviceWorker" in navigator) || !("PushManager" in window)) {
-        console.warn("Push not supported");
         return;
       }
 
       // register service worker (path must match your build)
-      const reg = await navigator.serviceWorker.register("/service-worker.js");
-      console.log("SW registered");
+      const reg = await navigator.serviceWorker.register("/service-worker.js",{scope:'/'});
 
       // request notification permission
       const permission = await Notification.requestPermission();
       if (permission !== "granted") {
-        console.warn("Push permission not granted");
         return;
       }
-
-      // get VAPID public key from server or settings endpoint
-      const r = await fetch("http://localhost:8000/api/notifications/vapid/");
-      const { publicKey } = await r.json();
-
       const subscription = await reg.pushManager.subscribe({
         userVisibleOnly: true,
-        applicationServerKey: urlBase64ToUint8Array(publicKey),
+        applicationServerKey: urlBase64ToUint8Array(PPK),
       });
-
+      console.log(subscription);
       // send subscription to server
       await fetch("http://localhost:8000/api/notifications/subscribe/", {
         method: "POST",
