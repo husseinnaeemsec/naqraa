014b019e1d82d33e0895abca284a4fc9d26307d0
Hussein Naeem <husseinnaeemsec@gmail.com>
2025-11-12 13:06:09 +0300

    removing user cache logic.

--- src/store/authSlice.tsx
+++ src/store/authSlice.tsx
@@ -1,67 +1,62 @@
 import { createSlice, type PayloadAction } from '@reduxjs/toolkit';
-import type {Enrollment, InitialAuthState, Notification, StudyTimeWeek, User} from '../../types';
-import { getUserProfile } from '../utils/functions';
+import type { Enrollment, InitialAuthState, Notification, StudyTimeWeek, User } from '../../types';
 
 // Get initial state from localStorage if available
-
-
-
-
-
-const initialState : InitialAuthState = {
+const initialState: InitialAuthState = {
   user: null,
   isAuthenticated: false,
   // Setting this value to true allows Protected Routes to wait until the AuthProvider loades the user
   // And then set this back to false 
   loadingUser: true,
   authError: [],
-  enrollments:[],
-  ready_for_notifications:false,
-  notifications:[],
-  week_study_time:null
+  enrollments: [],
+  ready_for_notifications: false,
+  notifications: [],
+  week_study_time: null
 };
 
 const authSlice = createSlice({
   name: 'auth',
   initialState,
   reducers: {
-    setNotificationsState:(state,action:PayloadAction<boolean>)=>{
+    setNotificationsState: (state, action: PayloadAction<boolean>) => {
       state.ready_for_notifications = action.payload;
     },
-    setWeekStudyTime:( state,action:PayloadAction<StudyTimeWeek|null> )=>{
+    setWeekStudyTime: (state, action: PayloadAction<StudyTimeWeek | null>) => {
       state.week_study_time = action.payload;
 
     },
-    setNotifications:( state , action:PayloadAction<Notification[]> )=>{
+    setNotifications: (state, action: PayloadAction<Notification[]>) => {
       state.notifications = action.payload;
     },
-    setAuthenticationState:(state,action:PayloadAction<boolean>)=>{
+    setAuthenticationState: (state, action: PayloadAction<boolean>) => {
       state.isAuthenticated = action.payload;
     },
-    setUser:(state,action:PayloadAction<User|null>)=>{
+    setUser: (state, action: PayloadAction<User | null>) => {
       state.user = action.payload;
-      if(!getUserProfile() && state.user?.profile){
-        localStorage.setItem("profile",JSON.stringify(state.user.profile))
-      }
+      state.week_study_time = action.payload?.week_study_time || null 
+
     },
-    setLoadingState:( state , action:PayloadAction<boolean> )=>{
+    setLoadingState: (state, action: PayloadAction<boolean>) => {
       state.loadingUser = action.payload;
     },
-    logoutUser:(state)=>{
-      state.loadingUser = true
-      state.user = null;
+    logoutUser: (state) => {
       state.isAuthenticated = false;
-      localStorage.removeItem("profile");
+      state.user = null;
+      state.enrollments = [];
+      state.notifications = [];
+      localStorage.removeItem("user_cache");
       state.loadingUser = false;
     },
-    setUserEnrollments:(state,action:PayloadAction<Enrollment[]>)=>{
+
+    setUserEnrollments: (state, action: PayloadAction<Enrollment[]>) => {
       state.enrollments = action.payload;
     },
-    appendEnrollment:(state,action:PayloadAction<Enrollment>)=>{
-      state.enrollments = [...state.enrollments,action.payload]
+    appendEnrollment: (state, action: PayloadAction<Enrollment>) => {
+      state.enrollments = [...state.enrollments, action.payload]
     }
   },
 });
 
-export const { setAuthenticationState,setWeekStudyTime,setNotifications,setNotificationsState,setUserEnrollments,appendEnrollment,setUser,logoutUser, setLoadingState } = authSlice.actions;
+export const { setAuthenticationState, setWeekStudyTime, setNotifications, setNotificationsState, setUserEnrollments, appendEnrollment, setUser, logoutUser, setLoadingState } = authSlice.actions;
 export default authSlice.reducer;
