2fa959333c7d941892063f0af6f21aa264bb88cd
Hussein Naeem <husseinnaeemsec@gmail.com>
2025-11-14 22:30:31 +0300

    useLanguage hook.

---
+++ src/hooks/use-langauge.ts
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react"
+import { useAppSelector } from "../store/store";
+
+export const useLangaugeHook = ()=>{
+    const [langauge,setLangauge] = useState<'en'|'ar'|'ku'>('en');
+    const [loading,setLoading] = useState(false)
+    const {user,isAuthenticated} = useAppSelector(state=>state.auth);
+    useEffect(()=>{
+        setLoading(true)
+        if(isAuthenticated && user?.profile?.lang){
+            setLangauge(user.profile.lang)
+        }else{
+            setLangauge( ( localStorage.getItem('lang') as 'ar'|'en'|'ku' )  || 'ar' )
+        }
+        setLoading(false)
+    },[isAuthenticated,user?.profile?.lang])
+
+
+    return { loading,langauge }
+}
